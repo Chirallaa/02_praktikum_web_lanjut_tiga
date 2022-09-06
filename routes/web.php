8a3956bdba35f6c1238e97e3b4eae193fafcec08
= <gr523517@gmail.com>
2022-09-06 21:34:07 +0700

    Praktikum web lanjut tiga

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeCompanyController;
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ContactUsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,11 +23,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('.Home', [HomeCompanyController::class, 'index']);
-
-Route::prefix('category')->group(function()) {
-    Route::get('/{id}', [ProductsController::class, 'products']);
+Route::get('/Home', function () {
+   echo '<h1>Halaman Awal Webiste</h1>';
 });
 
+Route::prefix('category')->group(function () {
+Route::get('/{id}', [ProductsController::class, 'products']);
 });
+
+Route::get('/news/{id?}', [NewsController::class, 'news']);
+
+Route::prefix('program')->group(function () {
+Route::get('/{id}', [ProgramController::class, 'program']);
+});
+
+Route::get('/AboutUs', [AboutUsController::class, 'about']);
+
 Route::resource('contactUs', ContactUsController::class);
